87a121c8b2e9a2f65eca5a15e6962d22470b297a
cyll99 <christyanlove@gmail.com>
2023-05-18 13:22:30 -0700

    program can download the first file

--- chemninDeVie.py
+++ chemninDeVie.py
@@ -16,11 +16,12 @@ for i in range(1, 2):
   
     # Find the container of every emission
     containers = soup.find_all("div", {"class" : "liste-emissions"})
-    print(containers)
     for container in containers:
         link = container.find_all('a')
         # print(link)
         page_audio = link[0]["href"]
+
+        print("page audio: ", page_audio)
 
         #Open the web page of each emission
         linked_response = requests.get(page_audio)
@@ -29,21 +30,29 @@ for i in range(1, 2):
         # Find all <a> tags with an href attribute that ends with ".mp3"
         mp3_links = linked_soup.find_all('source', src=lambda src: src.endswith('.mp3'))
 
+        directory = 'audio_files\\deuteronome'
         # Create a directory to store the downloaded files
-        if not os.path.exists('audio_files'):
-            os.makedirs('audio_files')
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
 
         file_url = mp3_links[0]["src"]
+
         file_name = file_url.split('/')[-1]
-        file_path = os.path.join('C:\\Users\\Mydleyka\\Documents\\sideProjects\\deuteronome', file_name)
+        file_path = os.path.join(directory, file_name)
+
+            # Check if the file already exists
+        if os.path.exists(file_path):
+            print(f'Skipped {file_name} - File already exists')
+            continue
+
 
         try:
             with open(file_path, 'wb') as file:
+                    print(f"Downloadind {file_name}....")
                     response = requests.get(file_url)
-                    print(file_url)
                     file.write(response.content)
                     
                     print(f'{file_name} downloaded')
         except:
-            print("Somethings went wrong")
+            print("Somethings went wrong. File not downloaded")
